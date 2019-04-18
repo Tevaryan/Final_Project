17041693635bd71ae18764281f0e69bddb224371
Mohamad Alkel <arcmohammadalkl@gmail.com>
2019-04-18 12:20:04 +0800

    fix log in

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -45,6 +45,7 @@ class SignUp extends React.Component{
 
     render(){
 
+<<<<<<< Updated upstream
       
 
       return(
@@ -83,6 +84,47 @@ class SignUp extends React.Component{
   
   
   
+=======
+      if (this.state.login == true){
+        return <Redirect to='/Dashboard/TradeMain/MyFeed'/>
+      } else {
+        return(
+        <Modal.Dialog>
+                <Modal.Header closeButton>
+                <Modal.Title>Sign Up</Modal.Title>
+                </Modal.Header>
+            
+                <Modal.Body>
+                    <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Enter username" onChange={this.nameInputHandler}/>
+                            
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="plase enter valid email" onChange={this.emailInputHandler}/>
+                            <Form.Text className="text-muted">
+                              We'll never share your email with anyone else.
+                            </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" onChange={this.passwordInputHandler} pattern="[\dA-Za-z]\w{8,14}$"/>
+                            <Form.Text className="text-muted">
+                              Password must contain more than 8 words
+                            </Form.Text>
+                    </Form.Group>
+                </Modal.Body>
+            
+                <Modal.Footer>
+                <Button variant="primary" type="submit" onClick={this.handleSubmit}>Sign Up</Button>
+                <Button variant="secondary">Cancel</Button>
+                </Modal.Footer>
+        </Modal.Dialog>
+      )
+      }
+    }
+>>>>>>> Stashed changes
 }
 
 export default SignUp;
